017f0b7f4f436ce88a09fa6e4236cc57b80272c4
Timothy J. Aveni <timothy@tacosareawesome.com>
2016-07-09 23:28:08 -0400

    Add Angular and some interface

--- index.js
+++ index.js
@@ -1,57 +1,69 @@
-window.onload = function () {
-	var audioCtx = new window.AudioContext();
-	var waveform;
+var audioCtx = new window.AudioContext();
+var wavelyricApp = angular.module('wavelyricApp', []);
 
-	var dropZone = document.body;
+wavelyricApp.controller('WavelyricCtrl', function ($scope) {
+	$scope.stage = 'start';
+	$scope.dropZone = document.getElementById('dropZone');
+	$scope.tab = 'metadata';
 
+	$scope.metadata = {
+		language: 'EN',
+		difficulty: '1'
+	};
+
+	registerEventListeners($scope.dropZone, $scope);
+});
+
+var registerEventListeners = function (dropZone, $scope) {
 	dropZone.ondragover = function (e) {
 		e.stopPropagation();
 		e.preventDefault();
-		
-		e.dataTransfer.dropEffect = 'copy';
+
+		e.dataTransfer.dropEffect = 'copy';	
 	};
 
 	dropZone.ondragenter = function (e) {
-		document.body.style.backgroundColor='#dfd';
+		if (e.target === $scope.dropZone) {
+			$scope.draggingFile = true;
+			$scope.$apply();
+		}
 	};
 
 	dropZone.ondragleave = function (e) {
-		document.body.style.backgroundColor='white';
+		if (e.target === $scope.dropZone) {
+			$scope.draggingFile = false;
+			$scope.$apply();
+		}
 	};
 
 	dropZone.ondrop = function (e) {
 		e.stopPropagation();
 		e.preventDefault();
 
-		document.body.style.backgroundColor='white';
+		$scope.draggingFile = false;
 
 		var files = e.dataTransfer.files;
 		if (files.length > 0) {
-			var file = files[0];
-			
+			let file = files[0];
+
+			// die if it's not an audio file
 			if (!(file.type.length > 6 && file.type.substring(0, 6) === 'audio/')) {
 				return;
 			}
 
-			var reader = new FileReader();
-			reader.onload = function (e) {
-				audioCtx.decodeAudioData(e.target.result).then(function (audioBuffer) {
-					let waveform = new Waveform(audioBuffer, 1000);
-					editor = new MarkerEditor(document.getElementById('waveCanvas'), audioCtx, waveform, 0, waveform.length);
+			let reader = new FileReader();
 
-					document.onkeydown = e => {
-						if (e.keyCode === 32) {
-							if (editor.playing) {
-								editor.stop();
-							} else {
-								editor.play();
-							}
-						}
-					};
+			reader.onload = function (readerEvent) {
+				audioCtx.decodeAudioData(readerEvent.target.result).then(function (audioBuffer) {
+					$scope.waveform = new Waveform(audioBuffer, 1000);
+					$scope.stage = 'loaded';
+					$scope.$apply();
 				});
 			};
 
 			reader.readAsArrayBuffer(file);
+			$scope.stage = 'loading';
+			$scope.$apply();
 		}
-	};
-};
+	}
+}
